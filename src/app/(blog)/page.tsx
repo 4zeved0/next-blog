fa24710ccdc6b06c7f3733aa4528c6b5fa926865
4zeved0 <azevedo.mdos@outlook.com>
2024-05-23 21:58:54 -0300

    Improved post search functionality.

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -3,16 +3,23 @@ import Posts from "../components/Posts"
 import { sanityFetch } from "../../../sanity/lib/fetch"
 import { POSTS_QUERY } from "../../../sanity/lib/queries"
 import TrendingPost from "../components/TrendingPost"
+import SearchPost from "../components/SearchPost"
 
 export default async function Page() {
   const posts = await sanityFetch<SanityDocument[]>({
     query: POSTS_QUERY,
   })
+
+  let filteredPosts = posts.map((post) => {
+    return post.title
+  })
+
+
   return <>
     <div className="grid grid-cols-3 gap-10">
       <div className="col-span-2">
         <h1 className="text-3xl font-bold mt-10">Recentes</h1>
-        <Posts posts={posts} />
+        <SearchPost posts={posts} />
       </div>
       <div>
         <h1 className="text-3xl font-bold mt-10">Em alta</h1>
---
+++ src/app/components/SearchPost.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { useState } from 'react';
+import { SanityDocument } from "next-sanity";
+import Link from 'next/link';
+import { format } from 'date-fns';
+
+export default function SearchPost({ posts }: { posts: SanityDocument[] }) {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPosts: SanityDocument[] = posts.filter((post) => {
+    return post.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  let postsToDisplay: SanityDocument[];
+  if (searchTerm === '') {
+    postsToDisplay = posts.slice(0, 2);
+  } else {
+    postsToDisplay = filteredPosts;
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-center mt-5 border-transparent mb-5">
+        <input
+          type="text"
+          placeholder="Pesquisar"
+          className="outline-none p-2 border border-slate-300 w-full rounded-md"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+
+      <ul>
+        {postsToDisplay.map((post) => {
+          const formattedDate: string = format(new Date(post._createdAt), 'dd/MM/yyyy');
+
+          return (
+
+            <li key={post.id} className='max-h-40'>
+              <Link href={post.slug.current}>
+                <div className="py-4 px-2 bg-slate-300 rounded-md mb-2 hover:bg-slate-400 flex justify-between">
+                  <h2>{post.title}</h2>
+                  <p className="text-sm text-slate-300 bg-slate-600 rounded-md px-2 py-1">postado em: {formattedDate}</p>
+                </div>
+              </Link>
+            </li>
+          )
+        }
+        )
+        }
+      </ul >
+    </>);
+}
